80e959228e8acbc1793b5add923f82ca1ddeb70f
Fernando Aguilar <320045031@pcpuma.acatlan.unam.mx>
2024-11-25 10:19:21 -0600

    Initial Commit

---
+++ hackharmony/hackharmonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackharmonyApp.swift
+//  hackharmony
+//
+//  Created by Fernando Aguilar on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct hackharmonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hackharmonyTests/hackharmonyTests.swift
@@ -0,0 +1,17 @@
+//
+//  hackharmonyTests.swift
+//  hackharmonyTests
+//
+//  Created by Fernando Aguilar on 25/11/24.
+//
+
+import Testing
+@testable import hackharmony
+
+struct hackharmonyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
